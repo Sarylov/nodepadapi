85264b3ceca45b2c70cafd495755dd375fcb4451
Animushnik <100656969+Animushnik@users.noreply.github.com>
2022-07-07 07:15:42 +0300

    new commit

--- db.js
+++ db.js
@@ -1,10 +1,13 @@
 const Pool = require("pg").Pool;
 const pool = new Pool({
-  user: "postgres",
-  password: "123124321",
-  host: "localhost",
-  port: "5432",
-  database: "notepad",
+  // localhost db
+  // user: "postgres",
+  // password: "123124321",
+  // host: "localhost",
+  // port: "5432",
+  // database: "notepad",
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
 });
 
 module.exports = pool;
